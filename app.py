d8878be5fbd143b1af2a1c675f38156d9d367444
Shivam Aggarwal <shivamaggarwal1821@gmail.com>
2024-07-14 20:26:46 +0530

    khlhk

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from nselib import capital_market
 
-try:
-    print("Testing API call...")
-    data = capital_market.market_watch_all_indices()
-    if data is None or 'last' not in data or 'indexSymbol' not in data:
-        raise ValueError("Unexpected response format from API")
-    print("API call successful. Data:", data)
-except Exception as e:
-    print("Error during API call:",e)
+def main():
+    try:
+        print("Testing API call...")
+        data = capital_market.market_watch_all_indices()
+        if data is None or 'last' not in data or 'indexSymbol' not in data:
+            raise ValueError("Unexpected response format from API")
+        print("API call successful. Data:", data)
+    except Exception as e:
+        print("Error during API call:",e)
+
+
+if __name__ == "__main__":
+    main()
