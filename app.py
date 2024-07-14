be14ebdaf5f8ddfdbf8c28185b69f04d77366770
Shivam Aggarwal <shivamaggarwal1821@gmail.com>
2024-07-14 19:25:07 +0530

    dlsjkf

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, jsonify
-from nselib import Nse
+from nsepython import nse_eq
+from nselib import capital_market
+from flask_cors import CORS
 
 app = Flask(__name__)
-nse = Nse()
+CORS(app)
+
+def get_indices():
+    try:
+        data = capital_market.market_watch_all_indices()
+        last = data['last']
+        index_symbol = data['indexSymbol']
+        result = []
+        for sym, lst in zip(index_symbol, last):
+            result.append({
+                'last': int(lst),
+                'indexSymbol': sym
+            })
+        return result
+    except Exception as e:
+        return {'error': str(e)}
+
 
 @app.route('/api/get_stock_info', methods=['GET'])
-def get_stock_info():
-    stock_data = nse.get_quote('TCS')
-    return jsonify(stock_data)
+def indices():
+    response_data = get_indices()
+    return jsonify(response_data)
+
